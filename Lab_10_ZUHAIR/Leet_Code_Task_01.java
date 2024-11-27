c9b1a0dcb986c2cc4d2f8e7ec879a4b7078a4d2d
shahzuhair73@gmail.com <shahzuhair73@gmail.com>
2024-11-27 19:54:14 +0500

    Uploading labs

---
+++ Lab_10_ZUHAIR/Leet_Code_Task_01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class Leet_Code_Task_01 {
+     if (root == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null || t1.val != t2.val) return false;
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        return true;
+}
---
+++ Lab_10_ZUHAIR/Task_05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Task_05 {
+     class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    List<List<Integer>> result =new ArrayList<>();
+    if(root==null){
+        return result;
+    }
+    findpaths(root, targetSum, ){
+
+    }
+}
